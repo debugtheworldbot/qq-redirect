8b78bf00bf87c016fe292ecb7015a1c734d2aa9a
tian <istiancz@gmail.com>
2024-04-26 15:43:55 +0800

    feat: add secret

--- app/[proj]/api/route.ts
+++ app/[proj]/api/route.ts
@@ -3,7 +3,11 @@ import { sql } from "@vercel/postgres";
 export async function POST(request: Request) {
   const body = await request.json();
   console.log("body", body);
-  const { date, link, type } = body;
+  const { date, link, type, secret } = body;
+
+  if (secret !== process.env.SECRET) {
+    return new Response("invalid secret", { status: 403 });
+  }
 
   await sql`INSERT INTO Links (date, link, type ) VALUES (${date}, ${link}, ${type} )`;
   return new Response("POST OK", { status: 200 });
